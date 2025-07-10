56862f1efd2f65ffddcfd36b5b0ffb0681f16e40
SamJohn04 <samuel.john.codes@gmail.com>
2025-07-10 15:26:12 +0530

    refactor: add core.py

--- organizer/main.py
+++ organizer/main.py
@@ -1,8 +1,7 @@
-import time
 import argparse
 from pathlib import Path
 
-from organizer import config
+from organizer import core, config
 
 
 def main():
@@ -31,7 +30,7 @@ def search_and_move():
             }
 
     try:
-        search_dir(dir_to_search, dest_dirs)
+        core.search_dir(dir_to_search, dest_dirs)
     except AssertionError as e:
         print(e)
     except Exception as e:
@@ -62,41 +61,6 @@ def write():
             }
 
     config.write_config((dir_to_search, dest_dirs))
-
-
-def search_dir(dir_path: Path, dest_dirs: dict[str, Path]) -> None:
-    """
-    Search through a directory and move the files that match.
-    """
-    assert dir_path.is_dir(), f"{dir_path} is not a directory."
-
-    for child_path in dir_path.iterdir():
-        if child_path.is_file() and child_path.suffix in dest_dirs:
-            move_file(child_path, dest_dirs[child_path.suffix])
-
-
-def move_file(src_path: Path, dest_dir: Path) -> None:
-    """
-    Moves a file. If a file with the same name exists, append timestamp.
-    """
-    # Create directory if it does not exist
-    if not dest_dir.exists():
-        dest_dir.mkdir(parents=True)
-
-    # Throw an error if not a directory
-    assert dest_dir.is_dir(), f"{dest_dir} is not a directory."
-
-    file_name = src_path.name
-    dest_path = dest_dir / file_name
-
-    # If name already exists
-    if dest_path.exists():
-        timestamp = int(time.time())
-        dest_path = dest_dir / f"{dest_path.stem}_{timestamp}{dest_path.suffix}"
-
-    # Move the file
-    src_path.rename(dest_path)
-    print(f"Moved: {src_path} -> {dest_path}")
 
 
 if __name__ == '__main__':
